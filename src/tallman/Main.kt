babb0577fa8b822491331396f8248ebcf502bdd1
euro <phuchit@omise.co>
2018-06-03 11:12:17 +0700

    Add example map

--- src/tallman/Main.kt
+++ src/tallman/Main.kt
@@ -2,16 +2,27 @@ package tallman
 
 private val map1: Array<IntArray> = arrayOf(
     intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
-    intArrayOf(0, 1, 0, 1, 1, 1, 1, 0),
+    intArrayOf(1, 1, 0, 1, 1, 1, 1, 0),
     intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
-    intArrayOf(0, 1, 1, 1, 1, 1, 1, 0),
+    intArrayOf(0, 1, 1, 1, 1, 1, 1, 1),
     intArrayOf(0, 0, 0, 0, 1, 0, 0, 0),
-    intArrayOf(1, 0, 1, 1, 0, 0, 1, 1),
+    intArrayOf(1, 0, 1, 1, 0, 1, 1, 0),
+    intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+    intArrayOf(0, 1, 1, 1, 0, 1, 1, 0)
+)
+
+private val map2: Array<IntArray> = arrayOf(
+    intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+    intArrayOf(1, 1, 0, 1, 1, 1, 1, 0),
+    intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+    intArrayOf(0, 1, 1, 1, 1, 1, 1, 1),
+    intArrayOf(0, 0, 0, 0, 1, 0, 1, 0),
+    intArrayOf(1, 0, 1, 1, 0, 1, 1, 0),
     intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
     intArrayOf(0, 1, 1, 1, 0, 1, 1, 0)
 )
 
 fun main(whatever: Array<String>) {
-    val rat = TallManRat.throwInMaze(map = map1, food = 1 to 0, position = 0 to 0)
-    println(rat.think(SpeakOptions.BEST_ROUTE))
+    val rat = TallManRat.throwInMaze(map = map1, food = 4 to 5, position = 0 to 0)
+    println(rat.think(SpeakOptions.ALL_ROUTE))
 }
